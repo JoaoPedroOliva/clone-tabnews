e1ab09722f3cc5537133fb22f6db083868021979
João Pedro Severo Oliva <joaopedro.oliva10@gmail.com>
2025-01-20 23:36:47 -0300

    Add `version` and `max_connections` of postgres

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  //Version
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValeu = databaseVersionResult.rows[0].server_version;
+
+  //Conexões máximas
+  const databaseMaxConnections = await database.query("SHOW max_connections");
+  const databaseMaxConnectiosValue =
+    databaseMaxConnections.rows[0].max_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValeu,
+        max_connections: databaseMaxConnectiosValue,
+      },
+    },
+  });
 }
 
 export default status;
